1dca9c0c385d40770734b2ead7217c22891234c3
Elikem Adadevoh <elikem@gmail.com>
2014-04-21 18:21:35 -0700

    adds copy and paste functionality

--- lib/githubrepo.rb
+++ lib/githubrepo.rb
@@ -67,6 +67,14 @@ module Githubrepo
         puts clone_url
         Clipboard.copy clone_url
         puts "If xclip is installed, repository URL has been added to your clipboard."
+        puts "debian/ubuntu: apt-get install xclip"
+      # elsif OS.windows?
+      #   Clipboard.copy clone_url
+      #   # Will Clipboard output clone_url to console if ffi is not installed?
+      #   # Below is what it looks like when Clipboard requirements are met
+      #   # https://github.com/user/*.git  ---  COPIED TO CLIPBOARD 
+      #   puts "If ffi is installed, repository URL has been added to your clipboard."
+      #   puts "for installation: gem install ffi"
       else
         puts clone_url
       end
